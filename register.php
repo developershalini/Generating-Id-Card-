dce3504b290ec1f16780cc490bea12864015511f
developershalini <shalini012002@gmail.com>
2024-07-05 15:42:08 +0530

    Upload email field

--- register.php
+++ register.php
@@ -10,44 +10,45 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-
 $name = $_POST['name'];
 $category = $_POST['category'];
+$email = $_POST['email'];
 $id_number = $_POST['id_number'];
-$city = $_POST['city'];
+$phoneno = $_POST['phoneno'];
 $district = $_POST['district'];
 $state = $_POST['state'];
 $pincode = $_POST['pincode'];
-$sql = "INSERT INTO registrations (name, category, id_number, city, district, state, pincode)
-        VALUES ('$name', '$category', '$id_number', '$city', '$district', '$state', '$pincode')";
+$sql = "INSERT INTO registrations (name, category, id_number,email, phoneno, district, state, pincode)
+        VALUES ('$name', '$category', '$id_number', '$email', '$phoneno', '$district', '$state', '$pincode')";
 if ($conn->query($sql) === TRUE) {
     $registration_id = $conn->insert_id;
     $pdf = new TCPDF();
     $pdf->AddPage();
-    $img_file = 'image/frontpageidcard.jpg';
+    $img_file = 'image/idcardfront.jpg';
     $image_width = 179;
     $page_width = 210;
     $x_center = ($page_width - $image_width) / 2;
     $pdf->Image($img_file, $x_center, 0, $image_width, 297);
-    $pdf->SetFont('Helvetica', '', 20);
+    $pdf->SetFont('Helvetica', 'B', 20);
     $pdf->SetTextColor(255, 255, 255);
-    $pdf->SetXY(75, 132);
-    $pdf->Write(0, "$name");
-    $pdf->SetXY(74, 147);
-    $pdf->Write(0, "$id_number");
-    $pdf->SetXY(89, 161);
-    $pdf->Write(0, "$category");
-    $pdf->SetXY(66, 177);
-    $pdf->Write(0, "$city");
-    $pdf->SetXY(81, 191);
-    $pdf->Write(0, "$district");
-    $pdf->SetXY(73, 204);
-    $pdf->Write(0, "$state");
-    $pdf->SetXY(83, 218);
-    $pdf->Write(0, "$pincode");
+    $pdf->SetXY(60, 128);
+    $pdf->Write(0, "Name: $name");
+    $pdf->SetXY(60, 140);
+    $pdf->Write(0, "Id No.: $id_number");
+    $pdf->SetXY(60, 152);
+    $pdf->Write(0, "Category: $category");
+    $pdf->SetXY(60, 164);
+    $pdf->Write(0, "Email: $email");
+    $pdf->SetXY(60, 176);
+    $pdf->Write(0, "District: $district");
+    $pdf->SetXY(60, 188);
+    $pdf->Write(0, "State: $state");
+    $pdf->SetXY(60, 200);
+    $pdf->Write(0, "Pin Code: $pincode");
+    $pdf->SetXY(60, 212);
+    $pdf->Write(0, "Phone No: $phoneno");
     $pdf->AddPage();
-    $img_file_back = 'image/back.jpeg';
+    $img_file_back = 'image/idcardback.jpg';
     $pdf->Image($img_file_back, $x_center, 0, $image_width, 297); // Adjust height as needed
    $pdf->Output('id_card.pdf', 'D');
     $pdf->close();
--- registration_form.php
+++ registration_form.php
@@ -9,21 +9,21 @@
     <form action="register.php" method="post" enctype="multipart/form-data">
         <label for="name">Name:</label>
         <input type="text" id="name" name="name" required><br><br>
-        <label for="category">Category:</label>
-        <input type="text" id="category" name="category" required><br><br>
         <label for="id_number">ID Number:</label>
         <input type="text" id="id_number" name="id_number" required><br><br>
-        <label for="city">City:</label>
-        <input type="text" id="city" name="city" required><br><br>
+        <label for="category">Category:</label>
+        <input type="text" id="category" name="category" required><br><br>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"><br><br>
         <label for="district">District:</label>
         <input type="text" id="district" name="district" required><br><br>
         <label for="state">State:</label>
         <input type="text" id="state" name="state" required><br><br>
         <label for="pincode">Pincode:</label>
         <input type="text" id="pincode" name="pincode" required><br><br>
-        <label for="image">Upload Image:</label>
-        <input type="file" id="upload" name="image" accept="image/*" required><br><br>
-        <input type="submit" name ="submit" value="Register" reset>
+        <label for="phoneno">Phone Number:</label>
+        <input type="tel" id="phoneno" name="phoneno" required pattern="[0-9]{10}"><br><br>
+         <input type="submit" name ="submit" value="Register" reset>
     </form>
 </body>
 </html>
